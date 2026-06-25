b04ec0297b5ab491169695172d987f314d4dddde
WUKUwork-+5 <1847940606@qq.com>
2026-06-25 18:30:29 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const supabaseUrl = 'https://qrfflkfcmiybgtcysupg.supabase.co';
-const supabaseKey = 'sb_publishable_WxEHfw7blMpchHp7EZOrRw_gGGQyo2H';
-
-const _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
