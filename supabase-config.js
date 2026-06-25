bce780cf430f1df94a5edb4890b307da15b07222
WUKUwork-+5 <1847940606@qq.com>
2026-06-25 18:47:23 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://qrfflkfcmiybgtcysupg.supabase.co';
+const supabaseKey = 'sb_publishable_WxEHfw7blMpchHp7EZOrRw_gGGQyo2H';
+
+const _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
